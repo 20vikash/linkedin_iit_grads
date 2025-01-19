692563340dbb3af7c4b95ce707751bedd10861a8
ssvikash_20 <arisivikash@gmail.com>
2025-01-19 13:29:59 +0530

    Add IIT alumini page URLs

--- read_env.py
+++ read_env.py
@@ -8,3 +8,18 @@ def get_password():
 
 def get_iit_delhi_url():
     return os.environ["IIT_DELHI"]
+
+def get_iit_bombay_url():
+    return os.environ["IIT_BOMBAY"]
+
+def get_iit_kanpur_url():
+    return os.environ["IIT_KANPUR"]
+
+def get_iit_guvahati_url():
+    return os.environ["IIT_GUVAHATI"]
+
+def get_iit_roorkee_url():
+    return os.environ["IIT_ROORKEE"]
+
+def get_iit_madras_url():
+    return os.environ["IIT_MADRAS"]
