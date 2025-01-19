2fe70e67d91077628aa1f16ad0ac43399b1a2869
ssvikash_20 <arisivikash@gmail.com>
2025-01-19 10:42:38 +0530

    Read email and password from env

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from read_env import *
 import time
 
 service = Service(executable_path="./chromedriver")
 driver = webdriver.Chrome(service=service)
 
-driver.get("https://google.com")
+email = get_email()
+password = get_password()
+
+driver.get("https://linkedin.com")
+
+signin_element = driver.find_element(By.CLASS_NAME, "nav__button-secondary")
+signin_element.click()
+
 time.sleep(10)
---
+++ read_env.py
@@ -0,0 +1,6 @@
+import os
+
+def get_email():
+    return os.environ["EMAIL"]
+def get_password():
+    return os.environ["PASSWORD"]
