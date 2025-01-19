909c4fd2145e94a1fa2fc1c003c85d66d8d2a428
ssvikash_20 <arisivikash@gmail.com>
2025-01-19 10:49:41 +0530

    Login to linkedin

--- main.py
+++ main.py
@@ -15,4 +15,13 @@ driver.get("https://linkedin.com")
 signin_element = driver.find_element(By.CLASS_NAME, "nav__button-secondary")
 signin_element.click()
 
+email_field = driver.find_element(By.ID, "username")
+password_field = driver.find_element(By.ID, "password")
+
+email_field.send_keys(email)
+password_field.send_keys(password)
+
+signin_button = driver.find_element(By.CLASS_NAME, "btn__primary--large")
+signin_button.click()
+
 time.sleep(10)
